eb2abf7d3cf896686febd2339f2625dc3224bf6f
fx000 <myself>
2021-04-19 13:06:25 +0200

    [i] Create types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,18 @@
-/// <reference types="react-scripts" />
+export type Shelter = {
+    id : Number,
+    name : String
+}
+
+export type Shelters = {
+    shelters : Shelter[]
+}
+
+export interface ContributionProps {
+    firstName : String,
+    lastName : String,
+    email : String,
+    value : String,
+    phone : String | undefined,
+    shelterID : Number | undefined
+}
+

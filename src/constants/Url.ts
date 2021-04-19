3e21cc5d675541f232ddf407e7c2f285ef751988
fx000 <myself>
2021-04-19 14:26:46 +0200

    [+] Add url constants + redux/toolkit dependency

---
+++ src/constants/Url.ts
@@ -0,0 +1,4 @@
+const BASE_URL : String = "https://frontend-assignment-api.goodrequest.com/api/v1/";
+
+export const SHELTERS = BASE_URL + "shelters";
+export const CONTRIBUTE = BASE_URL + "shelters/contribute";

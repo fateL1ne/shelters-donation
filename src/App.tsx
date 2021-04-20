107cbb2b780e1662038a01cb38790cf0dcfec328
fx000 <myself>
2021-04-20 12:31:46 +0200

    [+] Add redux provider for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import StepsPanel from './components/StepsPanel';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-      <>
-      "as"
-      </>
-  );
+    <Provider store={store}>
+      <StepsPanel />
+    </Provider>
+  );  
 }
 
 export default App;

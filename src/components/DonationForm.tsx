93901d41c39cad8a75f080f1017c877e9afba954
fx000 <myself>
2021-04-20 12:38:25 +0200

    [i] Create content & remove local state variables

--- src/components/DonationForm.tsx
+++ src/components/DonationForm.tsx
@@ -1,16 +1,63 @@
-import React, { useState } from 'react';
-import { Shelter } from './../react-app-env'; 
+import { Grid, Typography, Box } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Shelters, Shelter } from './../react-app-env'; 
+import { fetchShelters } from './../service/http/Shelter';
+import Select from '@material-ui/core/Select';
 
 
 export default function DonationForm() {
+    const [ shelters, setShelters ] = useState<Shelters | null>(null);
     const [ donationSum, setDonationSum ] = useState<Number>(0);
     const [ generalDonation, setGeneralDonation ] = useState<Boolean>(true);
     const [ chooseShelter, setChoosenShelter ] = useState<Shelter | null>(null);
 
 
+    useEffect(() => {
+        fetchShelters().then( (shelters : Shelters) => {
+            console.log(shelters);
+            setShelters(shelters);
+        }).catch( (err : Error) => {
+            console.log(err);
+        })
+    }, [])
+
     return (
-        <>
-        </>
+        <Grid container direction="column" spacing={3}>
+            <Grid item>
+            <Typography component="div">
+                <Box fontSize="h3.fontSize" fontWeight="fontWeightMedium" m={1}>
+                    Potrebujeme od Vás zopár informácií
+                </Box>
+            </Typography>
+            </Grid>
+            <Grid item>
+                neskor
+            </Grid>
+            <Grid item xs={9}>
+                <h3> Najviac mi záleží na útulku</h3>
+                <Select fullWidth/>
+            </Grid>
+            <Grid item>
+                <h3>Suma ktorou chcem prispiet</h3>
+                <Grid container spacing={3}>
+                    <Grid item>
+                        5e
+                    </Grid>
+                    <Grid item>
+                        10e
+                    </Grid>
+                    <Grid item>
+                        20e
+                    </Grid>
+                    <Grid item>
+                        30e
+                    </Grid>
+                    <Grid item>
+                        50e
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 
 }
--- src/components/PersonalForm.tsx
+++ src/components/PersonalForm.tsx
@@ -1,14 +1,48 @@
-import React, { useState } from 'react';
+import { Box, Grid, Typography } from '@material-ui/core';
+import React from 'react';
+import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import store from '../redux/store';
+import { setEmail, setFirstName, setLastName, setPhone } from '../redux/slices/User';
+import { State } from '../react-app-env';
 
 
-export default function PersonalForm() {
-    const [ firstName, setFirstName ] = useState<String>("");
-    const [ lastName, setLastName ] = useState<String>("");
-    const [ email, setEmail ] = useState<String>("");
-    const [ telephone, setTelephone ] = useState<String>("");
+function PersonalForm() {
+
+    const changeName = (event : React.ChangeEvent<HTMLInputElement>) => store.dispatch(setFirstName(event.target.value));
+    const changeSurname = (event : React.ChangeEvent<HTMLInputElement>) => store.dispatch(setLastName(event.target.value));
+    const changeEmail = (event : React.ChangeEvent<HTMLInputElement>) => store.dispatch(setEmail(event.target.value));
+    const changePhone = (event : React.ChangeEvent<HTMLInputElement>) => store.dispatch(setPhone(event.target.value));
+
 
     return (
-        <>
-        </>
+        <Grid container direction="row" justify="center" spacing={1}>
+            <Typography component="div">
+                <Box fontSize="h3.fontSize" fontWeight="fontWeightMedium" m={1}>
+                    Potrebujeme od Vás zopár informácií
+                </Box>
+            </Typography>
+            <Grid item xs={8}>
+                <TextField fullWidth id="outlined-basic" label="Meno" variant="outlined" onChange={changeName}/>
+            </Grid>
+            <Grid item xs={8}>
+                <TextField fullWidth id="outlined-basic" label="Priezvisko" variant="outlined"  onChange={changeSurname}/>
+            </Grid>
+            <Grid item xs={8}>
+                <TextField fullWidth id="outlined-basic" label="E-mailová adresa" variant="outlined" onChange={changeEmail}/>
+            </Grid>
+            <Grid item xs={8}>
+                <TextField fullWidth id="outlined-basic" label="Telefonné číslo" variant="outlined" onChange={changePhone}/>
+            </Grid>
+        </Grid>
     );
 }
+
+
+function mapStateToProps (state : State) {
+    return { 
+        user : state.user
+    }
+}
+
+export default connect(mapStateToProps)(PersonalForm)

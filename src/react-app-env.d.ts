32a26e2b688566cba17eeab8c8ef2e344dfe34f1
fx000 <myself>
2021-04-20 12:33:37 +0200

    [i] Add types & declare png module

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,6 @@
 export type Shelter = {
-    id : Number,
-    name : String
+    id : number,
+    name : string
 }
 
 export type Shelters = {
@@ -8,20 +8,53 @@ export type Shelters = {
 }
 
 export interface ContributionProps {
-    firstName : String,
-    lastName : String,
-    email : String,
-    value : String,
-    phone : String | undefined,
-    shelterID : Number | undefined
+    firstName : string,
+    lastName : string,
+    email : string,
+    value : string,
+    phone : string | void,
+    shelterID : number | void
 }
 
 export type Message = {
-    type: String,
-    message: String
+    type: string,
+    message: string
 }
 
 export type Messages = {
     messages : Message[]
+}
+
+export interface Donation {
+    general: boolean,
+    shelter?: Shelter | undefined,
+    amount: number
+}
+
+export interface UserStructure {
+    firstName : string,
+    lastName : string,
+    email : string,
+    phone : string 
+}
+
+export interface StepsState {
+    actualStep: number
+}
+
+export interface ButtonProps {
+    title : string,
+    callback: () => void;
+}
+
+export interface SummaryProps {
+    user : UserStructure,
+    donation : Donation
+}
+
+export interface State {
+    user : UserStructure,
+    donation : Donation,
+    steps: StepsState
 }
 
